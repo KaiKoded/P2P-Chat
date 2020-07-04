eb18278dcc24d10d700bf6a1ba529f6019356c62
Alpay <Alpay.yilmaz95@gmail.com>
2020-07-04 18:19:11 +0200

    Integrated chat

--- App.py
+++ App.py
@@ -1,65 +1,55 @@
 from appJar import gui
-# TODO instead of stopping, just clear
-app = gui("Peer2Peer Chat", "800x400")
-# add & configure widgets - widgets get a name, to help referencing them later
-app.addLabel("title", "P2P Chat")
-app.addLabelEntry("Username")
-app.addLabelEntry("Port")
-app.addLabelEntry("EntryPoint")
+import Chat
 
-chat_content = ""
 
+class App_UI(object):
+    gui = gui("Threading Chord", "800x400")
+    chat_content = ""
+    input_ready = False
+    port = 11111
+    quit = False
+
+app = App_UI()
+app.gui.addLabel("title", "P2P Chat")
+app.gui.addLabelEntry("Username")
+app.gui.addLabelEntry("Port")
+app.gui.addLabelEntry("EntryPoint")
 
 def login(button):
     global app
     if button == "Cancel":
-        app.stop()
+        app.gui.stop()
     else:
-        username = app.getEntry("Username")
-        port = app.getEntry("Port")
-        entry = app.getEntry("EntryPoint")
-
+        username = app.gui.getEntry("Username")
+        app.port = int(app.gui.getEntry("Port"))
+        entry = app.gui.getEntry("EntryPoint")
         connect_to_overlay(entry)
         
 def connect_to_overlay(entry):
     #TODO
     global app
-    app.stop()
+    app.gui.stop()
     # TODO Connect to overlay
-    app = gui("Peer2Peer Chat", "800x400")
-    app.addLabelEntry("Friend to connect")
-    app.addButtons(["Connect"], connect_to_friend)
-    app.go()
+    app.gui = gui("Peer2Peer Chat", "800x400")
+    app.gui.addLabelEntry("Friend to connect")
+    app.gui.addButtons(["Connect"], connect_to_friend)
+    app.gui.go()
 
 def connect_to_friend():
     global app
-    app.stop()
+    app.gui.stop()
+    friend_username = app.gui.getEntry("Friend to connect")
     # TODO Connect to friend peer
-    start_chat("TODO")
-
-
-def start_chat(friend_name):
-    global app 
-    app = gui("Peer2Peer Chat", "800x400")
-    app.startLabelFrame("Chat")
-    app.addEmptyMessage("chat_output")
-    app.addEntry("chat_input")
-    app.addButtons(["Send", "Quit"], chatting)
-    app.stopLabelFrame()
-    app.go()
+    friend_ip, friend_port = friend_username.split(':') # TODO
+    Chat.start_chat(app, friend_ip, int(friend_port))
 
 def chatting(button):
     global app
-    global chat_content
-    input = app.getEntry("chat_input")
-    app.clearEntry("chat_input")
-    chat_content = chat_content + "\n" + f"You wrote: {input}"
-    app.setMessage("chat_output", chat_content)
-    
+    input = app.gui.getEntry("chat_input")
+    app.gui.clearEntry("chat_input")
+    app.chat_content = app.chat_content + "\n" + f"You wrote: {input}"
+    app.gui.setMessage("chat_output", app.chat_content)
 
 
-
-
-app.addButtons(["Login", "Cancel"], login)
-
-app.go()
+app.gui.addButtons(["Login", "Cancel"], login)
+app.gui.go()
--- Chat.py
+++ Chat.py
@@ -1,26 +1,30 @@
 import socket
 import threading
 import time
+from appJar import gui
 
-print("Enter own port:")
-port = int(input())
-print("Enter partner ip: ")
-partner_ip = "localhost"
-print("Enter partner port:")
-partner_port = int(input())
+#print("Enter own port:")
+#port = int(input())
+#print("Enter partner ip: ")
+#partner_ip = "localhost"
+#print("Enter partner port:")
+#partner_port = int(input())
 
-chat_log = ""
+g_app = ""
 listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # This socket listens for the other peer's messages
 sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # This socket sends messages to the other peer
 
 
-def listening():
+def listening(app, port: int):
     """Listens to incoming Messages from Partner."""
     listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listening_socket.bind(('localhost', port))
     listening_socket.listen(1)
     (partner_connection, address) = listening_socket.accept()
     while True:
+        if app.quit:
+            listening_socket.close()
+            break
         try:
         # Receive Message from Partner
             data = partner_connection.recv(1024)
@@ -28,12 +32,14 @@ def listening():
             partner_connection.close()
         # Parse Message    
         message = str(data, "utf-8")
-        if message == "EXIT":
+        if message == "EXIT" or message == "":
             partner_connection.close()
             print("Partner disconnected.")
+        app.chat_content = app.chat_content + "\n" + f"Friend says: {message}"
+        app.gui.setMessage("chat_output", app.chat_content)
         print(f"Partner says: {message}")
 
-def sending(partner_ip:str, partner_port:int):
+def sending(app, partner_ip:str, partner_port:int):
     """Sends Messages to Partner."""
     print(f"Partner ip and port: {partner_ip}:{partner_port}")
 
@@ -50,26 +56,55 @@ def sending(partner_ip:str, partner_port:int):
     print("Enter Message:")
     # Send Messages
     while True:
-        
-        message = input()
-        if message == "" or message == "EXIT":
+        if app.quit:
             sending_socket.close()
             break
-        data = bytes(message, "utf-8")
-        try:
-            sending_socket.send(data)
-            print(f"You Said: {message}")
-        except socket.error:
-            sending_socket.close()
-            break
+        if app.input_ready:
+            app.input_ready = False
+            message = app.gui.getEntry("chat_input")
+            app.chat_content = app.chat_content + "\n" + f"You say: {message}"
+            if message == "" or message == "EXIT":
+                sending_socket.close()
+                break
+            data = bytes(message, "utf-8")
+            try:
+                sending_socket.send(data)
+                print(f"You Said: {message}")
+            except socket.error:
+                sending_socket.close()
+                break
+        time.sleep(0.1)
 
-def start_chat(partner_ip: str, partner_port: int):
+def chat_button(button):
+    global g_app
+    if button == "Quit":
+        g_app.quit = True
+        time.sleep(1)
+        g_app.gui.stop()
+    else:
+        g_app.input_ready = True
+
+def start_chat(app, partner_ip: str, partner_port: int):
+    # Prepare Chat
+    global g_app
+    g_app = app
+    app.gui = gui("Threading Chord Chat", "800x400")
+    app.gui.startLabelFrame("Chat")
+    app.gui.addEmptyMessage("chat_output")
+    app.gui.addEntry("chat_input")
+    app.gui.addButtons(["Send", "Quit"], chat_button)
+    app.gui.stopLabelFrame()
+    
+
+
     # Starting listening socket
-    listening_thread = threading.Thread(target=listening, daemon=True)
+    listening_thread = threading.Thread(target=listening, args=(app, app.port), daemon=True)
     listening_thread.start()
-    sending_thread = threading.Thread(target=sending, args=(partner_ip, partner_port), daemon=True)
+    sending_thread = threading.Thread(target=sending, args=(app, partner_ip, partner_port), daemon=True)
     sending_thread.start()
+    app.gui.go()
     sending_thread.join()
     listening_thread.join()
+    
 
-start_chat(partner_ip, partner_port)
+#start_chat(partner_ip, partner_port)
