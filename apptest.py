28da5290b41b6f548f9ffab1d1802bdc0d42e8c8
Alpay <Alpay.yilmaz95@gmail.com>
2020-07-12 18:30:19 +0200

    Tester

--- apptest.py
+++ apptest.py
@@ -1,7 +1,14 @@
 import ChordNoIP
 from time import sleep
 
+port_pre = 10000
+local_node = ChordNoIP.LocalNode({}, port_pre+1, "", "IchbinCool")
+number_of_nodes = 11
+local_nodes = []
+for number in range(3,number_of_nodes,2):
+    print(f"Number: {number}")
+    sleep(3)
+    local_nodes.append(ChordNoIP.LocalNode({}, port_pre+number, f"84.137.116.42:{port_pre+1}", str(port_pre+number)))
 
-local_node = ChordNoIP.LocalNode({}, 10, "127.0.0.1:19", "IchbinCool")
 while True:
     pass
